5035ed9c92439841ce6af54cc893a1f6f8be101a
moodler <moodler>
2005-07-17 02:51:50 +0000

    Moving Dialogue out of main tree into contrib again
       http://moodle.org/mod/forum/discuss.php?d=19017

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php // $Id: version.php,v 1.4 2005/07/17 02:51:51 moodler Exp $
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the module version etc.
+//  This fragment is called by /admin/index.php
+////////////////////////////////////////////////////////////////////////////////
+
+$module->version  = 2005031000;
+$module->requires = 2005031000;  // Requires this Moodle version
+$module->cron     = 60;
+
+?>
