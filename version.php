35558ce94a92b61f1dadb4516bcd9d720b5d3968
thepurpleblob <thepurpleblob>
2006-04-19 10:51:51 +0000

    Added support for database migration to utf8. Further testing required!

--- version.php
+++ version.php
@@ -1,11 +1,11 @@
-<?php // $Id: version.php,v 1.4 2005/07/17 02:51:51 moodler Exp $
+<?php // $Id: version.php,v 1.5 2006/04/19 10:51:51 thepurpleblob Exp $
 
 ////////////////////////////////////////////////////////////////////////////////
 //  Code fragment to define the module version etc.
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2005031000;
+$module->version  = 2006041900;
 $module->requires = 2005031000;  // Requires this Moodle version
 $module->cron     = 60;
 
