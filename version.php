61145893148c69a9960e751313d74ce696e3e918
Troy Williams <troyw@waikato.ac.nz>
2014-02-11 09:04:32 +1300

    Upped version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013101504;
+$module->version   = 2013101505;
 $module->requires  = 2013050100;        // See http://docs.moodle.org/dev/Moodle_Versions
 $module->cron      = 60;
 $module->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
