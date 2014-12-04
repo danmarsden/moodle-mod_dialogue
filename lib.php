2bfefb268697f6d35321eadff821a00ec43adb73
Troy Williams <troyw@waikato.ac.nz>
2014-12-05 09:46:46 +1300

    Removed deprecated dialogue feature FEATURE_GROUPMEMBERSONLY

--- lib.php
+++ lib.php
@@ -29,7 +29,6 @@ function dialogue_supports($feature) {
     switch($feature) {
         case FEATURE_GROUPS:                  return false;
         case FEATURE_GROUPINGS:               return false;
-        case FEATURE_GROUPMEMBERSONLY:        return false;
         case FEATURE_MOD_INTRO:               return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return false;
         case FEATURE_COMPLETION_HAS_RULES:    return false;
