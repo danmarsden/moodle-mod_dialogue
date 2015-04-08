14a50bb1f3a825a6a29896317863f0e633bd4dea
Troy Williams <troyw@waikato.ac.nz>
2015-04-08 14:03:31 +1200

    Removed redundant code from userpreference JavaScript

--- yui/src/userpreference/js/userpreference.js
+++ yui/src/userpreference/js/userpreference.js
@@ -1,11 +1,10 @@
 M.mod_dialogue = M.mod_dialogue || {};
 M.mod_dialogue.userpreference = {
     name : null,
-    redirect : null,
-    init : function(name, redirect) {
-        Y.one('#'+name).on('click', this.set_user_preference, this, name, redirect);
+    init : function(name) {
+        Y.one('#'+name).on('click', this.set_user_preference, this, name);
     },
-    set_user_preference : function(e, name, redirect) {
+    set_user_preference : function(e, name) {
         value = 0;
         if (Y.one('#'+name).get('checked')) {
             value = 1;
