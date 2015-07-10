f8d2bcac6386d30fedd1931cdb75b81ec2acff65
Troy Williams <troyw@waikato.ac.nz>
2015-07-10 18:56:58 +1200

    Added new fields, along with upgrade path

--- db/upgrade.php
+++ db/upgrade.php
@@ -20,8 +20,123 @@ function xmldb_dialogue_upgrade($oldversion=0) {
     $dbman = $DB->get_manager(); // Loads ddl manager and xmldb classes.
 
     /**
-     * Moodle v2.8.0 release upgrade line.
+     * Moodle v2.9.0 release upgrade line.
      */
+
+    if ($oldversion < 2014120309) {
+        // Get the conversation table.
+        $table = new xmldb_table('dialogue_conversations');
+
+        // New field owner.
+        $field = new xmldb_field('owner', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // New field instigator.
+        $field = new xmldb_field('instigator', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // New field recipient.
+        $field = new xmldb_field('recipient', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        // New field messagecount.
+        $field = new xmldb_field('messagecount', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // ** Gather and update conversation record data ** //
+        $rs = $DB->get_records('dialogue_conversations', array(), 'id DESC');
+        $progessbar = new progress_bar('dialogueupgrade', 500, true); $numberdone = 0; $total = count($rs);
+        foreach ($rs as $conversation) {
+            $params = array('conversationid'=>$conversation->id);
+            list($insql, $inparams) = $DB->get_in_or_equal(\mod_dialogue\dialogue::get_unread_states(), SQL_PARAMS_NAMED, 'un');
+            $params = array_merge($params, $inparams);
+            $messages = $DB->get_records_select('dialogue_messages', "conversationid = :conversationid AND state $insql",
+                $params, 'conversationindex');
+            $messagecount = count($messages);
+            if (!empty($messagecount)) {
+                $firstmessage = array_shift($messages);
+                $owner = $firstmessage->authorid;
+                $instigator = $firstmessage->authorid;
+                // Search for recipient.
+                if ($messagecount == 1) {
+                    $recipient = $firstmessage->authorid;
+                } else {
+                    $recipient = null;
+                    while (!empty($messages)) {
+                        $nextmessage = array_shift($messages);
+                        if ($firstmessage->authorid != $nextmessage->authorid) {
+                            $recipient = $nextmessage->authorid;
+                            break;
+                        }
+                    }
+                    if (is_null($recipient)) {
+                        $recipient = (int) $DB->get_field_select('dialogue_participants',
+                            'userid', "conversationid = :conversationid AND userid != :userid",
+                            array('conversationid'=>$conversation->id, 'userid'=>$firstmessage->authorid));
+                    }
+                }
+                $conversation->owner = $owner;
+                $conversation->instigator = $instigator;
+                $conversation->recipient = $recipient;
+                $conversation->messagecount = $messagecount;
+                // Add new data to conversation record.
+                $DB->update_record('dialogue_conversations', $conversation);
+
+                $numberdone++;
+                $progessbar->update($numberdone, $total, "Upgrading conversation data - {$numberdone}/{$total}");
+            }
+
+        }
+        upgrade_mod_savepoint(true, 2014120309, 'dialogue');
+    }
+
+    if ($oldversion < 2014120320) {
+        // Get the conversation table.
+        $table = new xmldb_table('dialogue_conversations');
+        // New field openrule.
+        $field = new xmldb_field('openrule', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        $sql = "SELECT dm.id AS messageid, dm.conversationid, dm.state, dr.cutoffdate, dr.lastrun
+                  FROM {dialogue_messages} dm
+                  JOIN {dialogue_bulk_opener_rules} dr ON dr.conversationid = dm.conversationid";
+        $rs = $DB->get_records_sql($sql);
+        foreach ($rs as $record) {
+            $DB->set_field('dialogue_conversations', 'openrule', 1, array('id'=>$record->conversationid));
+            if ($record->state == \mod_dialogue\dialogue::STATE_BULK_AUTOMATED) {
+                $lastrun = $record->lastrun;
+                $cutoffdate = $record->cutoffdate;
+                $state = \mod_dialogue\dialogue::STATE_OPEN;
+                if ((!$cutoffdate and $lastrun) or ($cutoffdate > time())) {
+                    $state = \mod_dialogue\dialogue::STATE_CLOSED;
+                }
+                $DB->set_field('dialogue_messages', 'state', $state, array('id'=>$record->messageid));
+            }
+        }
+        upgrade_mod_savepoint(true, 2014120320, 'dialogue');
+    }
+
+    if ($oldversion < 2014120321) {
+        $table = new xmldb_table('dialogue');
+        // New field opener limit field.
+        $field = new xmldb_field('oneperperson', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+        // Conditionally launch add field.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2014120321, 'dialogue');
+    }
 
     return true;
 }
