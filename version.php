77bb448a8ac4e2b1be38d329086766dcdc37cd28
Troy Williams <troyw@waikato.ac.nz>
2015-07-24 12:41:47 +1200

    Upping version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014071701;
+$plugin->version   = 2014071702;
 $plugin->requires  = 2014050800;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
 $plugin->release   = '2.7';             // Human-friendly version name
