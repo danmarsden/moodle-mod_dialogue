282fac59c48f9c8fa370fd9c06604d1b70b69c83
Troy Williams <maxorsomeness@gmail.com>
2015-12-12 16:16:50 +1300

    Upping version information for 3.0 release.

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014120300;
-$plugin->requires  = 2014111000;        // See http://docs.moodle.org/dev/Moodle_Versions
+$plugin->version   = 2015111600;
+$plugin->requires  = 2015111600;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
-$plugin->release   = '2.8';             // Human-friendly version name
-$plugin->maturity  = MATURITY_ALPHA;    // This version's maturity level
+$plugin->release   = '3.0';             // Human-friendly version name
+$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level
 $plugin->dependencies = array();
