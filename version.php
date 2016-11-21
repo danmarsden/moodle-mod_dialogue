1b063918dae5d0725b6dddef814bb140d9bc1cac
Troy Williams <maxorsomeness@gmail.com>
2016-11-21 18:41:14 +1300

    Set version file to 3.2.0-alpha

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016111600;
-$plugin->requires  = 2015111600;        // See http://docs.moodle.org/dev/Moodle_Versions
+$plugin->version   = 2016112100;
+$plugin->requires  = 2016111500;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
-$plugin->release   = '3.1';             // Human-friendly version name
-$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level
+$plugin->release   = '3.2.0-alpha';     // Semantic version name
+$plugin->maturity  = MATURITY_ALPHA;    // This version's maturity level
 $plugin->dependencies = array();
