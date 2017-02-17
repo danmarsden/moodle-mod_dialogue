2182ddc3768b5d4636d0058c5912aec24673c34f
Troy Williams <maxorsomeness@gmail.com>
2017-02-17 20:22:14 +1300

    Cleaned up backup_dialogue_stepslib.php based on codechecker recommendations.

--- backup/moodle2/backup_dialogue_stepslib.php
+++ backup/moodle2/backup_dialogue_stepslib.php
@@ -29,11 +29,10 @@ defined('MOODLE_INTERNAL') || die;
 class backup_dialogue_activity_structure_step extends backup_activity_structure_step {
     protected function define_structure() {
 
-        // To know if we are including userinfo
+        // To know if we are including userinfo.
         $userinfo = $this->get_setting_value('userinfo');
 
-
-        // Define each element separated
+        // Define each element separated.
         $dialogue = new backup_nested_element('dialogue', array('id'),
                                               array('course',
                                                     'name',
@@ -46,23 +45,17 @@ class backup_dialogue_activity_structure_step extends backup_activity_structure_
                                                     'notificationcontent',
                                                     'multipleconversations',
                                                     'timemodified'));
-  
         $conversations = new backup_nested_element('conversations');
-        
         $conversation = new backup_nested_element('conversation', array('id'),
                                                   array('course',
                                                         'dialogueid',
                                                         'subject'));
-        
         $participants = new backup_nested_element('participants');
-        
         $participant = new backup_nested_element('participant', array('id'),
                                                   array('dialogueid',
                                                         'conversationid',
                                                         'userid'));
-
         $bulkopenerrules = new backup_nested_element('bulkopenerrules');
-
         $bulkopenerrule = new backup_nested_element('bulkopenerrule', array('id'),
                                                      array('dialogueid',
                                                            'conversationid',
@@ -72,7 +65,6 @@ class backup_dialogue_activity_structure_step extends backup_activity_structure_
                                                            'cutoffdate',
                                                            'lastrun'));
         $messages = new backup_nested_element('messages');
-        
         $message = new backup_nested_element('message', array('id'),
                                               array('dialogueid',
                                                     'conversationid',
@@ -85,9 +77,7 @@ class backup_dialogue_activity_structure_step extends backup_activity_structure_
                                                     'state',
                                                     'timecreated',
                                                     'timemodified'));
-        
         $flags = new backup_nested_element('flags');
-
         $flag = new backup_nested_element('flag', array('id'),
                                            array('dialogueid',
                                                  'conversationid',
@@ -95,47 +85,35 @@ class backup_dialogue_activity_structure_step extends backup_activity_structure_
                                                  'userid',
                                                  'flag',
                                                  'timemodified'));
-
-        // Build the tree
+        // Build the tree.
         $dialogue->add_child($conversations);
         $conversations->add_child($conversation);
-        
         $conversation->add_child($participants);
         $participants->add_child($participant);
-        
         $conversation->add_child($bulkopenerrules);
         $bulkopenerrules->add_child($bulkopenerrule);
-        
         $conversation->add_child($messages);
         $messages->add_child($message);
-
         $conversation->add_child($flags);
         $flags->add_child($flag);
-        
-        // Define sources
+        // Define sources.
         $dialogue->set_source_table('dialogue', array('id' => backup::VAR_ACTIVITYID));
-        // All these source definitions only happen if we are including user info
+        // All these source definitions only happen if we are including user info.
         if ($userinfo) {
             $conversation->set_source_table('dialogue_conversations', array('dialogueid' => backup::VAR_PARENTID));
             $participant->set_source_table('dialogue_participants', array('conversationid' => backup::VAR_PARENTID));
-            // Leaving out bulk open rule as unsure how to annotate as could be courseid or groupid
-            //$bulkopenerrule->set_source_table('dialogue_bulk_opener_rules', array('conversationid' => backup::VAR_PARENTID));
             $message->set_source_table('dialogue_messages', array('conversationid' => backup::VAR_PARENTID));
             $flag->set_source_table('dialogue_flags', array('conversationid' => backup::VAR_PARENTID));
-            
         }
-
-        // Define id annotations
+        // Define id annotations.
         $participant->annotate_ids('user', 'userid');
         $message->annotate_ids('user', 'authorid');
         $flag->annotate_ids('user', 'userid');
-        
-        // Define file annotations
-        $dialogue->annotate_files('mod_dialogue', 'intro', null); // This file area hasn't itemid
+        // Define file annotations.
+        $dialogue->annotate_files('mod_dialogue', 'intro', null); // This file area hasn't itemid.
         $message->annotate_files('mod_dialogue', 'message', 'id');
         $message->annotate_files('mod_dialogue', 'attachment', 'id');
-                
-        // Return the root element, wrapped into standard activity structure
+        // Return the root element, wrapped into standard activity structure.
         return $this->prepare_activity_structure($dialogue);
     }
 }
