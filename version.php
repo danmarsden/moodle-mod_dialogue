3327ea154b1978355247a411dc325e27c36727f1
Troy Williams <maxorsomeness@gmail.com>
2017-12-09 15:42:01 +1300

    Release 3.3.0

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2017051500;
 $plugin->requires  = 2017051500;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
-$plugin->release   = '3.3.0-alpha';     // Semantic version name
-$plugin->maturity  = MATURITY_ALPHA;    // This version's maturity level.
+$plugin->release   = '3.3.0';           // Semantic version name
+$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level.
 $plugin->dependencies = array();
