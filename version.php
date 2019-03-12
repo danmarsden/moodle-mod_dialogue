5dbc0af3e76d38f854b3ab137074b306e33909d1
Troy Williams <maxorsomeness@gmail.com>
2019-03-12 20:33:07 +1300

    Work on v3.5.x begins...

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2017111300;
 $plugin->requires  = 2017111300;        // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->component = 'mod_dialogue';    // Full name of the plugin (used for diagnostics)
-$plugin->release   = '3.4';             // Semantic version name
-$plugin->maturity  = MATURITY_STABLE;   // This version's maturity level.
-$plugin->dependencies = array();
+$plugin->release   = '3.5';             // Semantic version name
+$plugin->maturity  = MATURITY_ALPHA;    // This version's maturity level.
+$plugin->dependencies = [];
