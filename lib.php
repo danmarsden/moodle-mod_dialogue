c208256cfa3706ccdccba64020df21ac96421353
Troy Williams <maxorsomeness@gmail.com>
2019-05-05 14:07:24 +1200

    Add fontawesome icon map.

--- lib.php
+++ lib.php
@@ -44,6 +44,16 @@ function dialogue_supports($feature) {
 }
 
 /**
+ * Get icon mapping for font-awesome.
+ */
+function mod_dialogue_get_fontawesome_icon_map() {
+    return [
+        'mod_dialogue:i/filter' => 'fa-filter',
+        'mod_dialogue:i/sort'   => 'fa-sort',
+    ];
+}
+
+/**
  * Adds a dialogue instance
  *
  * Given an object containing all the necessary data,
