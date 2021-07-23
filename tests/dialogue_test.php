a8f76eb6856c49f53680f059cfe58259c757fbf9
Dan Marsden <dan@danmarsden.com>
2021-07-24 11:41:10 +1200

    add basic unit test for creating activity.

---
+++ tests/dialogue_test.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_dialogue;
+
+/**
+ * Class dialogue_test.
+ *
+ * @package    mod_dialogue
+ * @copyright  2021 Dan Marsden
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class dialogue_test extends \advanced_testcase {
+    /**
+     * Basic test for creating dialogue.
+     */
+    public function test_coursemodule_for_dialogue() {
+        $this->resetAfterTest();
+        $this->setAdminUser();
+        $course = $this->getDataGenerator()->create_course();
+        $dialogue = $this->getDataGenerator()->create_module('dialogue', array('course' => $course->id));
+    }
+}
