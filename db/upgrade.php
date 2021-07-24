2bc9153925dddebe5245ec32d9c682b6b21341cd
Dan Marsden <dan@danmarsden.com>
2021-07-24 13:19:16 +1200

    add upgrade script shell for plugins db validation.

---
+++ db/upgrade.php
@@ -0,0 +1,40 @@
+<?php
+// This file keeps track of upgrades to
+// the data module
+//
+// Sometimes, changes between versions involve
+// alterations to database structures and other
+// major things that may break installations.
+//
+// The upgrade function in this file will attempt
+// to perform all the necessary actions to upgrade
+// your older installation to the current version.
+//
+// If there's something it cannot do itself, it
+// will tell you what you need to do.
+//
+// The commands in here will all be database-neutral,
+// using the methods of database_manager class
+//
+// Please do not forget to use upgrade_set_timeout()
+// before any action that may take longer time to finish.
+
+/**
+ * Dialogue upgrade scripts.
+ *
+ * @package mod_dialogue
+ * @copyright 2021 Dan Marsden
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Dialogue upgrade script.
+ *
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_dialogue_upgrade($oldversion) {
+    return true;
+}
